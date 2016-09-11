f28fedaed85a5566e526bc525595a6b9f3ab01f6
Dave Sidas <ajax@underlight.com>
2016-09-11 18:39:27 -0400

    Account for possibility for Reflect to be set on someone that doesn't have the Art.

--- Underlight/cGameServer.cpp
+++ Underlight/cGameServer.cpp
@@ -2342,7 +2342,9 @@ void cGameServer::HandleMessage(void)
 			bool art_reflected = false;
 			if (player->flags & ACTOR_REFLECT) {
 				// 1-50% chance (starts at 1%, increases 1% every other art improvement, 5% per plateau)
-				if (rand()%100 <= (1+player->Skill(Arts::REFLECT))/2)
+				// sets to level 49 if the player does not have the art (set by item)
+				int skillCheck = player->Skill(Arts::REFLECT) == 0 ? 49 : player->Skill(Arts::REFLECT);
+				if (rand()%100 <= (1+ skillCheck)/2)
 					art_reflected = true;
 				if (art_reflected){
 						cNeighbor *n = actors->LookUpNeighbor(player_msg.SenderID());
