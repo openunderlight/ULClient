0bc8a578fd7218752430426da7b107252fbaeb04
Michael Acobas <michael.acobas@gmail.com>
2016-09-26 03:03:37 -0400

    'level' is a macro for agents, rename var to 'lvl'

--- Underlight/cArts.h
+++ Underlight/cArts.h
@@ -296,7 +296,7 @@ public:
 	void ApplySoulShield(int skill, lyra_id_t caster_id);
 	void EndSoulShield(void);
 	void StartSummon(void);
-	void ApplySummon(lyra_id_t caster_id, int x, int y, int level);
+	void ApplySummon(lyra_id_t caster_id, int x, int y, int lvl);
 	void MidSummon(void);
 	void EndSummon(void *value);
 	void StartSuspend(void);
