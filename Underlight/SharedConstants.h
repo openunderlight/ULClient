1195804f0b0dc0fb5696ccda58739647dd2d3c93
Dave Sidas <ajax@underlight.com>
2017-04-23 09:15:39 -0400

    Updating default keybindings

--- Underlight/SharedConstants.h
+++ Underlight/SharedConstants.h
@@ -273,9 +273,7 @@ struct Arts {
   CHAOTIC_VORTEX, // Chaotic Vortex
   CHAOS_WELL, // Essence Container
   RALLY, // Summon party member
-  CHANNEL,     
-	BLADE, //placeholder
-	FLAME, // placeholder
+  CHANNEL,
 	// END OF ARTS LIST - below are art-related constants
 
 	INITIATE_DRAIN = 100,   // amount of essences drained from prime to Initiate
--- Underlight/resource.h
+++ Underlight/resource.h
@@ -2965,6 +2965,8 @@
 #define IDS_SCROLL_POLE                 4410
 #define IDS_NOACCESS_PORTAL             4450
 #define IDS_NO_XP_MONSTER               4451
+#define IDS_FOCAL_BLADE                 4452
+#define IDS_FOCAL_FLAME                 4453
 #define IDC_NUKE_PARTICLES              4802
 #define IDC_PARTICLE_RELOAD             4803
 #define IDC_AVATAR_COMBO                16666
