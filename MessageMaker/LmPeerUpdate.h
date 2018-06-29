cb281731a2d7fd369cb3cc880ff1c32bf5801b22
ajax <ajax@underlight.com>
2018-06-29 19:45:11 -0400

    Fixed my illiteracy

--- MessageMaker/LmPeerUpdate.h
+++ MessageMaker/LmPeerUpdate.h
@@ -58,10 +58,9 @@ public:
     // bitfield widths for u1
     ANGLE_WIDTH = 10,        // 10
     ATTACKBITS_WIDTH = 5,    // 15
-    FLAGS_WIDTH = 9,         // 24
-    WEAPONDAMAGE_WIDTH = 6,  // 30
-	FLIGHT_WIDTH = 1,		 // 31
-	UNUSED_WIDTH_U1 = 1,	 // 32
+    FLAGS_WIDTH = 10,         // 25
+    WEAPONDAMAGE_WIDTH = 6,  // 31
+	FLIGHT_WIDTH = 1,		 // 32
 
     // bitfield widths for u2
     WEAPONBITMAP_WIDTH = 3,    // 3
@@ -74,7 +73,7 @@ public:
     PRIMARY_COLOR_WIDTH = 3,   // 27
     SECONDARY_COLOR_WIDTH = 3, // 30
     WAVE_WIDTH = 1,			   // 31 - U2 has sacrificed a bit for the flags
-    //UNUSED_WIDTH_U2 = 1,	   // 32
+    UNUSED_WIDTH_U2 = 1,	   // 32
 
     // starting positions for u1 (shouldn't need to be modified)
     ANGLE_START = 0,
